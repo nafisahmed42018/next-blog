24dfa892a2576f183e38671cbc763c7059a3bed7
Nafis Ahmed <nafisahmed@iut-dhaka.edu>
2023-10-01 21:42:20 +0600

    navbar dropdown menu v2

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -21,18 +21,17 @@ const Navbar = () => {
             <li>
               <a href="#">Home</a>
             </li>
-            <li className="relative peer">
-              <div>Category</div>
-              <ul className="absolute -left-4 top-12 bg-background border rounded-lg w-32 peer-hover:block hidden ">
-                {category.map((item) => (
-                  <li
-                    className="px-2 py-2 hover:bg-secondary cursor-pointer"
-                    key={item.id}
-                  >
-                    {item.name}
-                  </li>
-                ))}
-              </ul>
+            <li>
+              <div className="relative group">
+                <div className="py-4">Category</div>
+                <ul className="absolute -left-8 bg-background w-[200%] hidden group-hover:block border-2 rounded-lg shadow-lg">
+                  {category.map((item) => (
+                    <li className="px-4 py-2 text-center hover:first:rounded-t-lg hover:last:rounded-b-lg hover:bg-primary cursor-pointer">
+                      {item.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </li>
             <li>
               <ToggleMode />
